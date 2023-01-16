0149965ce5b8f5e18ed4ebcd5327c32e6dc4f0b8
Luckysingh12345 <119068031+Luckysingh12345@users.noreply.github.com>
2023-01-16 17:09:16 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 echo "Cloning Repo...."
-git clone https://github.com/LazyDeveloperr/LazyUrlHunterrBOT.git /LazyUrlHunterrBOT
-cd /LazyUrlHunterrBOT
+git clone https://github.com/Luckysingh12345/Mdisk-link-finder.git /Mdisk-link-finder
+cd /Mdisk-link-finder
 pip3 install -U -r requirements.txt
 echo "⚡️⚡️ Starting LazyUrlHunterrBOT..."
 python3 main.py
